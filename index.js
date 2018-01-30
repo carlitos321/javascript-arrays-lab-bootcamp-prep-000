d31a8404ba5d8defb0f1815d881975afab97e97f
carlitos321 <ca.cabrera321@gmail.com>
2018-01-30 20:12:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,3 +19,15 @@ function destructivelyRemoveFirstKitten() {
 function appendKitten(name) {
   return [...kittens, name]
 }
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function removeLastKitten() {
+  return kittens.slice(kittens.length-1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
